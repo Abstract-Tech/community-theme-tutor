4dd06786a71eceaa2c2c08a4098926e91ada2cd4
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2023-03-03 09:57:51 +0100

    Remove env directory from project

--- env/apps/openedx/settings/cms/test.py
+++
@@ -1,5 +0,0 @@
-from cms.envs.test import *
-
-# Fix MongoDb connection credentials
-DOC_STORE_CONFIG["user"] = None
-DOC_STORE_CONFIG["password"] = None
--- env/apps/openedx/settings/lms/test.py
+++
@@ -1,5 +0,0 @@
-from lms.envs.test import *
-
-# Fix MongoDb connection credentials
-DOC_STORE_CONFIG["user"] = None
-DOC_STORE_CONFIG["password"] = None
