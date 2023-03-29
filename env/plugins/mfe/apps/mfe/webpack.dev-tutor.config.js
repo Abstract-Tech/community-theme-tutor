a39bddfe6866ac6ce9ec32747d10382eb11620a9
Mehmood ul Hassan <m.uihassan@abstract-technology.de>
2023-03-29 12:04:40 +0200

    apply translation on learning MFE

---
+++ env/plugins/mfe/apps/mfe/webpack.dev-tutor.config.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const fs = require('fs');
+
+const baseDevConfig = (
+  fs.existsSync('./webpack.dev.config.js')
+    ? require('./webpack.dev.config.js')
+    : require('@edx/frontend-build/config/webpack.dev.config.js')
+);
+
+module.exports = merge(baseDevConfig, {
+  // This configuration needs to be defined here, because CLI
+  // arguments are ignored by the "npm run start" command
+  devServer: {
+    // We will have to make changes to thix config in later releases of webpack dev devServer
+    // https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md
+    allowedHosts: 'all',
+    proxy: {
+      '/api/mfe_config/v1' : 'http://local.overhang.io:8000',
+    }
+  },
+})
+
