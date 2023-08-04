5dcac414ee42e65ed1199d5572c6772e03ff86bc
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2023-08-04 10:33:16 +0200

    feat: add missing mfes

--- plugins/community-mfe.py
+++ plugins/community-mfe.py
@@ -67,7 +67,20 @@ def mfe_forks(mfes):
         "port": 1995,
         "name": "profile",
     }
-    print(mfes)
+    mfes["home"] = {
+        "repository": "https://github.com/Abstract-Tech/community-theme-home",
+        "version": "community-theme.palm",
+        "refs": "https://api.github.com/repos/abstract-tech/community-theme-home/git/refs/heads",
+        "port": 1995,
+        "name": "home",
+    }
+    mfes["course_about"] = {
+        "repository": "https://github.com/Abstract-Tech/community-theme-course-about",
+        "version": "community-theme.palm",
+        "refs": "https://api.github.com/repos/abstract-tech/community-theme-course-about/git/refs/heads",
+        "port": 1995,
+        "name": "course_about",
+    }
     return mfes
 
 
@@ -76,9 +89,9 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "mfe-dockerfile-post-npm-install",
             """
-            RUN npm install '@edx/frontend-component-header@git+https://git@github.com/abstract-tech/community-theme-header.git#community-theme.palm' --registry=$NPM_REGISTRY
-            RUN npm install '@edx/frontend-component-footer@git+https://git@github.com/abstract-tech/community-theme-footer.git#community-theme.palm' --registry=$NPM_REGISTRY
-            RUN npm install '@edx/brand@git+https://git@github.com/abstract-tech/community-theme-brand.git#community-theme' --registry=$NPM_REGISTRY
+RUN npm install '@edx/frontend-component-header@git+https://git@github.com/abstract-tech/community-theme-header.git#community-theme.palm' --registry=$NPM_REGISTRY
+RUN npm install '@edx/frontend-component-footer@git+https://git@github.com/abstract-tech/community-theme-footer.git#community-theme.palm' --registry=$NPM_REGISTRY
+RUN npm install '@edx/brand@git+https://git@github.com/abstract-tech/community-theme-brand.git#community-theme' --registry=$NPM_REGISTRY
             """,
         ),
     ]
